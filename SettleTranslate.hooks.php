847e9b88c33fa3091b4fd58c882e8dee1fec1a0d
Vedmaka <god.vedmaka@gmail.com>
2016-06-01 04:37:24 +0300

    + alert on translation

--- SettleTranslate.hooks.php
+++ SettleTranslate.hooks.php
@@ -22,4 +22,14 @@ class SettleTranslateHooks
 		$parser->setFunctionHook('translate_link', 'SettleTranslate::renderLink', SFH_OBJECT_ARGS);
 	}
 
+	public static function onsfHTMLBeforeForm( &$targetTitle, &$html )
+	{
+		global $wgRequest, $wgOut;
+		if( $wgRequest->getCheck('translateMode') && $wgRequest->getCheck('translateFrom') ) {
+			$lang = $wgRequest->getVal('translateFrom');
+			$wgOut->addModules('ext.settletranslate.badge');
+			$html .= '<div class="alert alert-warning" role="alert">'.wfMessage( 'settle-translate-form-warning', Language::fetchLanguageName( $lang ) )->plain().'</div>';
+		}
+	}
+
 }
