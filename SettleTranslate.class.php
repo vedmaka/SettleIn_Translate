6c6bcc228cc68a6b351887bb9927664a87ca1898
Vedmaka <god.vedmaka@gmail.com>
2016-06-01 04:24:36 +0300

    + test

--- SettleTranslate.class.php
+++ SettleTranslate.class.php
@@ -55,8 +55,14 @@ class SettleTranslate {
 		return array(
 			$html,
 			'isHTML'  => true,
-			'noparse' => true
+			'noparse' => true,
+			'markerType' => 'none'
 		);
+	}
+
+	public static function makeInput( $name, $value )
+	{
+		return '<input type="hidden" name="'.$name.'" value="'.str_replace('"', "", str_replace( "\n", "", $value ) ).'" />';
 	}
 
 	/**
@@ -75,6 +81,190 @@ class SettleTranslate {
 
 		$page = WikiPage::factory( $parser->getTitle() );
 		$pageText = $page->getContent()->getWikitextForTransclusion();
+
+		//Title
+		$matches = array();
+		$match = preg_match('#\|Title=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Title]', $matches[1] );
+		}
+
+		//Country
+		$matches = array();
+		$match = preg_match('#\|Country=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Country]', $matches[1] );
+		}
+
+		//Country
+		$matches = array();
+		$match = preg_match('#\|Tags=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Tags]', $matches[1] );
+		}
+
+		//Application name
+		$matches = array();
+		$match = preg_match('#\|Application name=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Application name]', $matches[1] );
+		}
+
+		//Link to application
+		$matches = array();
+		$match = preg_match('#\|Link to application=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Link to application]', $matches[1] );
+		}
+
+		//Total cost
+		$matches = array();
+		$match = preg_match('#\|Total cost=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Total cost]', $matches[1] );
+		}
+
+		//Total cost currency
+		$matches = array();
+		$match = preg_match('#\|Total cost currency=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Total cost currency]', $matches[1] );
+		}
+
+		//Processing time
+		$matches = array();
+		$match = preg_match('#\|Processing time=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Processing time]', $matches[1] );
+		}
+
+		//Difficulty
+		$matches = array();
+		$match = preg_match('#\|Difficulty=(.+)#', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Difficulty]', $matches[1] );
+		}
+
+		// ...
+
+		// Tips - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card tips([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card tips['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		// Step by step - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card step by step([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card step by step['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		// Documents - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card documents([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card documents['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		// Costs - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card costs([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card costs['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		// Card links - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card links([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card links['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		// Card forms - multiple
+		$matches = array();
+		preg_match_all('/\\{\\{Card forms([^\\}\\}]+)/m', $pageText, $matches);
+		if( count($matches) > 1 ) {
+			foreach ( $matches[1] as $templateNumber => $itemContent ) {
+				$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
+				$matches2 = array();
+				preg_match_all($re, $itemContent, $matches2);
+				if( count($matches2) > 2 ) {
+					foreach ($matches2[0] as $i => $matchGroup) {
+						$paramName = $matches2[1][$i];
+						$paramValue = $matches2[2][$i];
+						$html .= self::makeInput( 'Card forms['.($templateNumber + 1).']['.$paramName.']', $paramValue );
+					}
+				}
+			}
+		}
+
+		//Content
+		$matches = array();
+		$match = preg_match('/\\|Content=([^\\|]+)/m', $pageText, $matches);
+		if( $match ) {
+			$html .= self::makeInput( $templateName.'[Content]', $matches[1] );
+		}
+
+		return $html;
+
+		//TODO: for the future versions
 
 		$re = "/\\|([^=]+)=([^\\|^\\}]+)/m";
 		$matches = array();
