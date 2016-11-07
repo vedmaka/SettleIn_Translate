c5c185a5735f3136fd13fbf025921f8227a5830d
Vedmaka <god.vedmaka@gmail.com>
2016-11-07 13:08:22 +0300

    - article new layout - related modifications in other extensions

--- SettleTranslate.class.php
+++ SettleTranslate.class.php
@@ -60,7 +60,7 @@ class SettleTranslate {
 		$parser->getOutput()->addModules( 'ext.settletranslate.window' );
 
 		$html .= $parser->insertStripItem(
-			'<a id="translate-link" href="#" type="button" class="btn btn-cyanide pull-right" data-toggle="modal" data-target=".translate-window"><i class="fa fa-language" ></i> '
+			'<a id="translate-link" href="#" type="button" class="card-special-skin-link" data-toggle="modal" data-target=".translate-window"> '
 			. wfMessage( 'settle-translate-link-text' )->plain() . '</a>' );
 
 		$html .= '<div id="translate-window" class="modal fade bs-example-modal-sm translate-window" role="dialog">';
